2a9730afd9fd4c7562ca85e0a96d7ee211a053ca
Aravinendh <aravinendhr@gmail.com>
2025-07-03 23:10:02 +0530

    employee added

---
+++ src/main/java/com/example/demo/services/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.example.demo.services;
+
+import com.example.demo.models.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class EmployeeService {
+    public List<Employee> getAllEmployees() {
+        return Arrays.asList(
+                new Employee(1, "Prasanth", "Trainer"),
+                new Employee(2, "Yuvaraj", "Faculty"),
+                new Employee(3, "Aravinendh", "Developer")
+        );
+    }
+}
