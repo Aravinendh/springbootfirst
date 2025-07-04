104c1702fe1ef4cf5b9c7afa7036c8443e8914c2
Aravinendh <aravinendhr@gmail.com>
2025-07-04 14:17:11 +0530

    day3

--- src/main/java/com/example/demo/controllers/EmployeeController.java
+++ src/main/java/com/example/demo/controllers/EmployeeController.java
@@ -3,20 +3,24 @@ package com.example.demo.controllers;
 import com.example.demo.models.Employee;
 import com.example.demo.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
 
-    @GetMapping
+    @GetMapping("/getEmployees")
     public List<Employee> getAllEmployees(){
         return employeeService.getAllEmployees();
+    }
+
+    @PostMapping("/createEmployee")
+    public String createEmployee(@RequestBody Employee employee){
+        return employeeService.createEmployee(employee);
     }
 }
--- src/main/java/com/example/demo/services/EmployeeService.java
+++ src/main/java/com/example/demo/services/EmployeeService.java
@@ -3,16 +3,25 @@ package com.example.demo.services;
 import com.example.demo.models.Employee;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class EmployeeService {
+
+    List<Employee> employees=new ArrayList<>();
+
     public List<Employee> getAllEmployees() {
-        return Arrays.asList(
-                new Employee(1, "Prasanth", "Trainer"),
-                new Employee(2, "Yuvaraj", "Faculty"),
-                new Employee(3, "Aravinendh", "Developer")
-        );
+        return employees;
+    }
+
+    public String createEmployee(Employee employee){
+        boolean res= employees.add(employee);
+        if(res){
+            return "Employee Added Successfully!\n"+ employee.toString();
+        }else{
+            return "Failed to Create Employee";
+        }
     }
 }
