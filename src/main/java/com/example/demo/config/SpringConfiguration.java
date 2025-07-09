98d6a95509d667b89169213c4baadc883695ab3f
Aravinendh <aravinendhr@gmail.com>
2025-07-09 15:18:02 +0530

    day5-an

--- src/main/java/com/example/demo/config/SpringConfiguration.java
+++ src/main/java/com/example/demo/config/SpringConfiguration.java
@@ -1,21 +1,23 @@
 package com.example.demo.config;
-
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringConfiguration {
-    // // to convert to pop-up
-//    @Bean
-//    public PasswordEncoder passwordEncoder(){          //BCryptPasswordEncoder hashes passwords for secure storage.
-//        return new BCryptPasswordEncoder();
-//    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){          //BCryptPasswordEncoder hashes passwords for secure storage.
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean//This class will define beans (objects managed by Spring).
     public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
         http.
@@ -23,5 +25,23 @@ public class SpringConfiguration {
                 .authorizeHttpRequests(auth->auth.anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    InMemoryUserDetailsManager userDetails(){
+        UserDetails admin= User.builder()//user1
+                .username("admin")
+                .password("admin")
+                .roles("ADMIN")
+                .build();
+
+        UserDetails Ar= User.builder()//user2
+                .username("Ar")
+                .password(passwordEncoder().encode("Ar"))//encoding the password using above  passwordEncoder()
+                .roles("USER")
+                .build();
+        //return admin;
+        return new InMemoryUserDetailsManager(admin,Ar);
+
     }
 }
